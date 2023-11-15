d5ce6b146059fcbb9189fedd84389c6973361cae
jurgeban <jurgen.voitenko@gmail.com>
2023-11-15 17:54:50 +0100

    15.11. added

---
+++ nov_15/js/script.js
@@ -0,0 +1,109 @@
+// ------------- 1 ----------------
+// let d = 7;
+// console.log('Значение переменной d = ', d);
+// console.log('Квадрат переменной d = ', d ** 2);
+// console.log('Квадрат переменной d = ', d ** 3);
+
+//--------------- 2 -----------------------
+// let num1 = Number(prompt("Enter the number"));
+// let num2 = Number(prompt("Enter the number"));
+
+// if (num1 > num2) {
+// 	console.log("Сумма этих чисел: ", num1 + num2);
+// } else if (num2 > num1) {
+// 	console.log("Произведение этих чисел: ", num2 - num1);
+// } else {
+// 	console.log("Числа одинаковые");
+// }
+
+//--------------- 3 ----------------------
+// let num = Number(prompt("Enter the number"));
+// if (num >= 0) {num = num ** 3;}
+// console.log(num);
+
+//--------------- 4 ----------------------
+// let pupils = Number(prompt("Enter the number"));
+// let chairs = Number(prompt("Enter the number"));
+
+// if (pupils <= chairs) {
+// 	console.log('Стульев хватает');
+// } else {
+// 	console.log("Стульев не хватает");
+// }
+
+ //-------------- 5 ------------------------
+// for (var i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+
+
+ //-------------- 6 ------------------------
+ // let array = [];
+ // for (var i = 0; i <= 20; i++) {
+ // 	array.push(i);
+ // }
+ // for (var i = 0; i < array.length; i++) {
+ // 	if (array[i] >= 15) {
+ // 	  console.log(array[i]);
+ // 	}
+ // }
+//-------------- 7 ------------------------
+ // let array = [];
+ // for (var i = 0; i <= 20; i++) {
+ // 	array.push(i);
+ // }
+ // for (var i = 0; i < array.length; i++) {
+ // 	if (array[i] % 2 != 0) {
+ // 	  console.log(array[i]);
+ // 	}
+ // }
+
+//-------------- 8 ------------------------
+ // let array = [];
+ // for (var i = 0; i <= 20; i++) {
+ // 	array.push(i);
+ // }
+ // for (var i = 0; i < array.length; i++) {
+ // 	  if (array[i] % 3 == 0) {
+ // 	  	console.log(array[i]);
+ // 	  }
+ // }
+
+//-------------- 9 ------------------------
+ // let array = [];
+ // for (var i = 0; i <= 20; i++) {
+ // 	array.push(i);
+ // }
+ // let oddSum = 0;
+ // for (var i = 0; i < array.length; i++) {
+ // 	if (array[i] % 2 != 0) {
+ // 	  oddSum += array[i];
+ // 	  console.log(array[i]);
+ // 	} 
+ // }
+ // console.log("Сумма нечетных чисел: ", oddSum);
+
+//-------------- 10 ------------------------
+//  let array = [];
+//  for (var i = 0; i <= 20; i++) {
+//  	array.push(i);
+//  }
+//  let even = 0;
+//  let odd = 0;
+//  for (var i = 0; i < array.length; i++) {
+//  	if (array[i] % 2 == 0) {
+//  	  even += array[i];
+//  	} else {
+//  	  odd += array[i];
+//  	}
+//  }
+// if (even > odd) {
+// 	console.log(`Сумма четных чисел на ${even - odd} больше нечетных`);
+// } else if(odd > even) {
+// 	console.log(`Сумма нечетных чисел на ${odd - even} больше четных`);
+// } else {
+//    console.log('Сумма четных и нечетных чисел одинакова');
+// }
+
+
+
